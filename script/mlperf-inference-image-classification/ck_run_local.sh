5dd83adfcb927465231098be05dd3cd7e9537b9b
Grigori Fursin <gfursin@tmoreau-hbv3-00.yzxataka1cwe1ha2s5sqddqvqb.bx.internal.cloudapp.net>
2021-07-14 08:58:16 +0000

    added user.conf support

--- script/mlperf-inference-image-classification/ck_run_local.sh
+++ script/mlperf-inference-image-classification/ck_run_local.sh
@@ -2,6 +2,13 @@
 
 echo "*********************************************************"
 
+${CK_ENV_COMPILER_PYTHON_FILE} -m pip freeze > ck-pip-freeze.txt
+ck > ck-version.txt
+
+if [ "x${CK_MLPERF_USER_CONF}" != "x" ] ; then
+  cp -f ${CK_MLPERF_USER_CONF} user.conf
+fi
+
 cd ${CK_ENV_MLPERF_INFERENCE_VISION}/classification_and_detection
 
 export DATA_DIR=${CK_ENV_DATASET_IMAGENET_VAL}
